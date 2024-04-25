9097a02250f91002250f4daa84abe02e551fbaae
meihwng <156946660+meihwng@users.noreply.github.com>
2024-04-25 18:46:59 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -60,7 +60,7 @@ function draw() {
     if(fruitGroup.isTouching(knife)){
       fruitGroup.destroyEach();
       
-      // knifeSwooshSound.play();
+       knifeSwooshSound.play();
       // knifeSwooshSound.play;
       // knifeSwooshSound();
       // knifeSwooshSoundplay();
@@ -69,7 +69,7 @@ function draw() {
       // score=score;
       // score=+2;
       // score=2;
-      // score=score+2;
+       score=score+2;
       
     }
     else
@@ -119,7 +119,7 @@ function fruits(){
     fruit.x = 0    
   //aumentar a velocidade das frutas após a pontuação 4 
 
-      // fruit.velocityX= (7+(score/4));
+       fruit.velocityX= (7+(score/4));
       // fruit.velocityY= (7+(score));
       // fruit.velocity= (7+(score/4));
       // fruit.velocityX= (7);
